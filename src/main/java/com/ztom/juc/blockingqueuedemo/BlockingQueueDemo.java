2922a10730a9e20373d40b0a419044b47e6cb94f
ZTom000 <ztmoon@qq.com>
2021-04-09 09:45:46 +0800

    20210409001 Update project ThreadPoolDemo BlockingQueueDemo StreamDemo Not finish

---
+++ src/main/java/com/ztom/juc/blockingqueuedemo/BlockingQueueDemo.java
@@ -0,0 +1,42 @@
+package com.ztom.juc.blockingqueuedemo;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueDemo {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+
+//        System.out.println(blockingQueue.add("a"));
+//        System.out.println(blockingQueue.add("b"));
+//        System.out.println(blockingQueue.add("c"));
+//
+//        System.out.println(blockingQueue.add("x")); // 异常抛出
+//
+//        System.out.println(blockingQueue.remove());
+//        System.out.println(blockingQueue.remove());
+//        System.out.println(blockingQueue.remove());
+//        System.out.println(blockingQueue.remove());
+
+//        System.out.println(blockingQueue.offer("a"));
+//        System.out.println(blockingQueue.offer("b"));
+//        System.out.println(blockingQueue.offer("c"));
+//
+//        // System.out.println(blockingQueue.offer("x"));
+//
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+
+        // 阻塞
+        blockingQueue.put("a");
+        blockingQueue.put("a");
+        blockingQueue.put("a");
+
+        blockingQueue.take();
+        blockingQueue.take();
+        blockingQueue.take();
+        blockingQueue.take();
+    }
+}
